5d425b3bb1779a305ed7da64e0f4f46e5bc26ac5
Pedro <pcroper1909@g.educaand.es>
2023-11-02 15:10:23 +0100

    menu cambiado

--- views/views/header.php
+++ views/views/header.php
@@ -3,9 +3,9 @@
     <div class="dropdown">
     <a href="?myAccount" class="dropbtn"><img src="sources/icons/accountCircle.svg" alt="account-icon"></a>
         <div class="dropdown-content">
-            <a href="?menu=login" class="btn">Login</a>
-            <a href="?menu=logout" class="btn">Logout</a>
-            <a href="?menu=myAccount" class="btn">My account</a>
+            <a href="?menu=login" class="dropbtn">Login</a>
+            <a href="?menu=logout" class="dropbtn">Logout</a>
+            <a href="?menu=myAccount" class="dropbtn">My account</a>
         </div>
     </div>
     <!--<a href="?myAccount"><img src="sources/icons/accountCircle.svg" alt="account-icon"></a>--> <!--<span><img src="sources/icons/accountCircle.svg" alt="account-icon"></span>-->
